e30ae49a977fdc863c8c1bcd82b8786965fcdd9a
jesal1234 <165134377+jesal1234@users.noreply.github.com>
2025-02-21 16:37:51 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,28 +1,5 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-                bat 'javac Helloworld.java'
-            }
-        }
-        stage('build') {
-            steps {
-                echo 'building in progress'
-            }
-        }
-        stage('test') {
-            steps {
-                echo 'testing in progress'
-            }
-        }
-        stage('deploy') {
-            steps {
-                echo 'deployment completed'
-                bat 'java Helloworld'
-            }
-        }
-    }
+public class test {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
